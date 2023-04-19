f7e422bb7160a2d3da154b749cc8df25013c480b
akashgond3112 <akashgond3112@gmail.com>
2023-04-19 22:46:17 +0100

    Added modal for adding review

---
+++ src/layouts/RestaurantPage/component/Review/Review.tsx
@@ -0,0 +1,183 @@
+import React, { useState } from "react";
+import Restaurant from "../../../../models/Restaurant/Restaurant";
+import { useReviewStyles } from "./Reviewmodule";
+
+export const Review: React.FC<{ resaurant: Restaurant }> = (props) => {
+  const classes = useReviewStyles();
+  const [showReviewModal, setshowReviewModal] = useState(false);
+  const [diningOptionSelected, setDiningOptionSelected] = useState(true);
+  const [deliveryOptionSelected, setDeliveryOptionSelected] = useState(false);
+
+  const handleDelieveryOption = () => {
+    setDeliveryOptionSelected(true);
+    setDiningOptionSelected(false);
+  };
+
+  const handleDiningOption = () => {
+    setDiningOptionSelected(true);
+    setDeliveryOptionSelected(false);
+  };
+  return (
+    <div className={classes.main}>
+      <div
+        className={
+          showReviewModal
+            ? `${classes.modalHolderHidden}`
+            : `${classes.modalHolderVisible}`
+        }
+        aria-hidden={showReviewModal ? `true` : `false`}
+        role="dialog"
+      >
+        <div
+          className={
+            showReviewModal
+              ? `${classes.modalBackgroundHidden}`
+              : `${classes.modalBackgroundVisible}`
+          }
+          tabIndex={-1}
+        ></div>
+        <div
+          className={
+            showReviewModal
+              ? `${classes.modalMainHidden}`
+              : `${classes.modalMainVisible}`
+          }
+          datatype="default"
+          tabIndex={0}
+        >
+          {/* Section 1 start */}
+          <section className={classes.addReviewHeaderMain}>
+            <div className={classes.addReviewHeaderContent}>Add Review</div>
+            <i
+              className={classes.addReviewCloseButtonPrimary}
+              color="#1C1C1C"
+              aria-label="close Modal"
+              tabIndex={0}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#1C1C1C"
+                width="24"
+                height="24"
+                viewBox="0 0 20 20"
+                aria-labelledby="icon-svg-title- icon-svg-desc-"
+                role="img"
+                className={classes.addReviewCloseButtonSecondary}
+              >
+                <title>cross</title>
+                <path d="M11.42 10.42l3.54-3.54c0.38-0.4 0.38-1.040 0-1.42s-1.020-0.4-1.42 0l-3.54 3.54-3.54-3.54c-0.4-0.4-1.020-0.4-1.42 0s-0.38 1.020 0 1.42l3.54 3.54-3.54 3.54c-0.38 0.38-0.38 1.020 0 1.42 0.2 0.18 0.46 0.28 0.72 0.28s0.5-0.1 0.7-0.28l3.54-3.56 3.54 3.56c0.2 0.18 0.46 0.28 0.72 0.28s0.5-0.1 0.7-0.28c0.38-0.4 0.38-1.040 0-1.42l-3.54-3.54z"></path>
+              </svg>
+            </i>
+          </section>
+          {/* Section 1 end */}
+          {/* Section 2 start */}
+          <section className={classes.addReviewHolderMain}>
+            <section className={classes.addReviewHolderInner}>
+              <h6 className={classes.addReviewRateExperienceContent}>
+                Rate your experience for
+              </h6>
+              <div className={classes.dineAndDeliveryOptionMain}>
+                <section className={classes.dineAndDeliveryOptionHolder}>
+                  {/* Dining Start */}
+                  <section className={classes.optionHolder}>
+                    <label className={classes.optionLabel}>
+                      <input
+                        name="radio"
+                        aria-label="Dining"
+                        // label="Dining"
+                        type="radio"
+                        value="dining"
+                        checked
+                        style={{ display: "none" }}
+                        onClick={handleDiningOption}
+                      ></input>
+                      <svg viewBox="0 0 20 20" className={classes.optionSvg}>
+                        {diningOptionSelected ? (
+                          <>
+                            <circle
+                              cx="10"
+                              cy="10"
+                              r="8"
+                              name="radio"
+                              className={classes.optionActiveOuterCircle}
+                            ></circle>
+                            <circle
+                              cx="10"
+                              cy="10"
+                              r="5"
+                              name="radio"
+                              className={classes.optionActiveInnerCircle}
+                            ></circle>
+                          </>
+                        ) : (
+                          <>
+                            <circle
+                              cx="10"
+                              cy="10"
+                              r="8"
+                              name="radio"
+                              className={classes.optionInActiveCircle}
+                            ></circle>
+                          </>
+                        )}
+                      </svg>
+                      <span className={classes.optionContent}>Dining</span>
+                    </label>
+                  </section>
+                  {/* Dining End */}
+                  {/* Delivery Start */}
+                  <section className={classes.optionHolder}>
+                    <label className={classes.optionLabel}>
+                      <input
+                        name="radio"
+                        aria-label="Delivery"
+                        type="radio"
+                        value="delivery"
+                        checked
+                        style={{ display: "none" }}
+                        onClick={handleDelieveryOption}
+                      ></input>
+                      <svg viewBox="0 0 20 20" className={classes.optionSvg}>
+                        {deliveryOptionSelected ? (
+                          <>
+                            <circle
+                              cx="10"
+                              cy="10"
+                              r="8"
+                              name="radio"
+                              className={classes.optionActiveOuterCircle}
+                            ></circle>
+                            <circle
+                              cx="10"
+                              cy="10"
+                              r="5"
+                              name="radio"
+                              className={classes.optionActiveInnerCircle}
+                            ></circle>
+                          </>
+                        ) : (
+                          <>
+                            <circle
+                              cx="10"
+                              cy="10"
+                              r="8"
+                              name="radio"
+                              className={classes.optionInActiveCircle}
+                            ></circle>
+                          </>
+                        )}
+                      </svg>
+                      <span className={classes.optionContent}>Delivery</span>
+                    </label>
+                  </section>
+                  {/* Delivery End */}
+                </section>
+              </div>
+            </section>
+          </section>
+          {/* Section 2 end */}
+        </div>
+      </div>
+    </div>
+  );
+};
